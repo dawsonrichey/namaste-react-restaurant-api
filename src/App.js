509fa0c7032a7253137c1228622d3b4dab13a702
dawsonrichey <dawsonrichey89@gmail.com>
2024-01-26 20:35:58 -0500

    added restaurant menu route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu";
 // import Contact from "./components/Contact";      
 
 const AppLayout = () => {
@@ -34,6 +36,11 @@ const appRouter = createBrowserRouter([
         path: "/contact",
         element: <Contact />,
       },
+    
+    {
+      path: "/resturants/:resId",
+      element: <RestaurantMenu/>,
+    },
     ],
     errorElement: <Error />,
   },
---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,15 @@
+const RestaurantMenu = () => {
+    return(
+        <div className="menu">
+<h1>Name of restaurant</h1>
+<h2>Menu</h2>
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+        </div>
+    );
+};
+
+export default RestaurantMenu;

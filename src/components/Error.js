2c29650b034be4a4b9eeeeb07c133a3c4171957c
dawsonrichey <dawsonrichey89@gmail.com>
2024-01-26 20:10:20 -0500

    added dynamic error message

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,15 @@
+import { useRouteError } from "react-router";
+
 const Error = () => {
+    const err = useRouteError();
+    console.log(err);
     return (
         <div>
             <h1>Error</h1>
             <h2>Opps something went wrong</h2>
+            <h3>
+                {err.status}: {err.statusText}
+            </h3>
         </div>
     );
 };

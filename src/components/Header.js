2fccdabe411264ac9b9a5ae353afa5f9a06d96a2
dawsonrichey <dawsonrichey89@gmail.com>
2024-01-26 20:24:15 -0500

    manually wrapper header text in links to page content

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,9 +16,9 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact Us</a></li>
           <li>Cart</li>
           <button
             className="login"

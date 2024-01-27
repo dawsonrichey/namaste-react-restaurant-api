4f2ba3692df48c17c31b5b102ce0dfbdb85808ba
dawsonrichey <dawsonrichey89@gmail.com>
2024-01-26 20:27:07 -0500

    removed useEffect and imported link function to route nav bar text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   console.log("Header render");
 
-  useEffect(() => {
-    console.log("useEffect called")
-  }, [btnNameReact]);
+  // useEffect(() => {
+  //   console.log("useEffect called")
+  // }, [btnNameReact]);
 
   return (
     <div className="header">
@@ -19,6 +20,7 @@ const Header = () => {
           <li><a href="/">Home</a></li>
           <li><a href="/about">About Us</a></li>
           <li><a href="/contact">Contact Us</a></li>
+          <li><Link to="/contact">Contact Us 2</Link></li>
           <li>Cart</li>
           <button
             className="login"
